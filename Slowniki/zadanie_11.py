80dcfb562d9df41519cc10c16e90bc7290b807f3
Krzysiek <tzeenchtof@o2.pl>
2019-01-12 13:08:50 +0100

    next

--- Slowniki/zadanie_11.py
+++ Slowniki/zadanie_11.py
@@ -3,5 +3,18 @@
 # w tym celu skorzystaj z operatora iloczynu
 
 
-unik_licz = input("Podaj unikalne liczby: ")
+# unik_licz = input("Podaj unikalne liczby: ")
 
+liczby = set()
+parzyste = set(range(0, 100, 2))
+while True:
+    komenda = input("Podaj liczbę lub [k] by zakońćzyć: ")
+    if komenda == 'k':
+        break
+    else:
+        liczby.add(int(komenda))
+
+# print(liczby)
+
+print("Liczbn unikalnych jest: ", len(liczby))
+print("Z tego parzystych jest: ", len(liczby & parzyste))
